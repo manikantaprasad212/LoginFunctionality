7644900010e750b8768edba705922a51d443e1c6
manikantaprasad212 <manikantaprasadprasadula@gmail.com>
2024-10-07 12:35:06 +0530

    Update Topnavigation.js

--- PostMethod-Urlencoded-Formdata-main/client/project/src/Components/Topnavigation.js
+++ PostMethod-Urlencoded-Formdata-main/client/project/src/Components/Topnavigation.js
@@ -1,7 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom'
 
 function Topnavigation() {
+  let navigate = useNavigate();
+
+    let storeObj = useSelector((store)=>{
+return store;
+    });
+
+    useEffect(()=>{
+        if(storeObj && storeObj.loginDetails && storeObj.loginDetails.email){
+
+        }else{
+            navigate("/");
+        }
+    },[])
+
   return (
    <nav>
     <Link to="/dashboard">DashBoard</Link>
